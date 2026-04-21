2f1d43fbbe2932b2247ca7498aa551e2eeb267b6
Amrudin Balić <amrudinbalic@192.168.1.136>
2026-04-21 18:42:25 +0200

    Categories Entity

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -2,24 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\JobPost;
 
+#[Fillable(['name', 'description', 'size', 'logo_url', 'website_url'])]
 class Company extends Model
 {
     /** @use HasFactory<\Database\Factories\CompanyFactory> */
     use HasFactory;
 
-    protected $fillable = [
-        'name',
-        'description',
-        'size',
-        'logo_url',
-        'website_url'
-    ];
-
+    /**
+     * Get Company Job Posts.
+     * @return HasMany<JobPost, Company>
+     */
     public function jobPosts(): HasMany
     {
         return $this->hasMany(JobPost::class);
