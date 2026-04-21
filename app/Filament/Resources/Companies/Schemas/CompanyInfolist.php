8aeb0812001bfc7f2f06cc02b028d90b0477d5f4
Amrudin Balić <amrudinbalic@192.168.1.136>
2026-04-21 13:59:01 +0200

    Filament -Company Resource

---
+++ app/Filament/Resources/Companies/Schemas/CompanyInfolist.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Resources\Companies\Schemas;
+
+use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Schema;
+
+class CompanyInfolist
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextEntry::make('name'),
+                TextEntry::make('description')
+                    ->placeholder('-')
+                    ->columnSpanFull(),
+                TextEntry::make('size'),
+                TextEntry::make('logo_url')
+                    ->placeholder('-'),
+                TextEntry::make('website_url')
+                    ->placeholder('-'),
+                TextEntry::make('created_at')
+                    ->dateTime()
+                    ->placeholder('-'),
+                TextEntry::make('updated_at')
+                    ->dateTime()
+                    ->placeholder('-'),
+            ]);
+    }
+}
