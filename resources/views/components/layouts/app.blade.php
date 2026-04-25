3819c11b19a741b825a7c0cd48c5db09f2216157
Amrudin Balić <amrudinbalic@192.168.1.136>
2026-04-25 12:47:12 +0200

    Initialize Flux

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="light">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
 
         <title>{{ $title ?? config('app.name') }}</title>
 
@@ -11,8 +14,10 @@
         @livewireStyles
     </head>
     <body>
+
         {{ $slot }}
 
         @livewireScripts
+        @fluxScripts
     </body>
 </html>
