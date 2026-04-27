45eb39958f77d215225988e537bb05507740d9ce
Amrudin Balić <amrudinbalic@192.168.1.136>
2026-04-27 18:40:41 +0200

    Vue

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import TextInput from '@/Components/TextInput.vue';
+import { Head } from '@inertiajs/vue3';
+
+const form = ref({
+    name: '',
+});
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2
+                class="text-xl font-semibold leading-tight text-gray-800"
+            >
+                Dashboard
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div
+                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
+                >
+                    <div class="p-6 text-gray-900">
+                        Welcome to your dashboard!
+
+                        <div>
+                            <InputLabel
+                                for="name"
+                                value="Name"
+                            />
+                            <TextInput 
+                                id="name"
+                                v-model="form.name"
+                                type="text"
+                                placeholder="Enter your name"
+                            />
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
