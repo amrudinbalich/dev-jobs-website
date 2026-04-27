96daef015825a301db1c1821d71c31a31fd9bf04
Amrudin Balić <amrudinbalic@192.168.1.136>
2026-04-27 09:09:28 +0200

    Remove React

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-// serve react app
-// Route::get('/{any}', function () {
-//     return view('app');
-// })->where('any', '.*');
